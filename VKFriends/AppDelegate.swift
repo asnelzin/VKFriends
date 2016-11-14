ae62ab079f667f7883c08fe7896f5133e71120f4
Alexander Nelzin <asnelzin@gmail.com>
2016-11-14 12:57:50 +0300

    Fix WebView auth;

--- VKFriends/AppDelegate.swift
+++ VKFriends/AppDelegate.swift
@@ -26,10 +26,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-//    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-//        VK.process(url: url, sourceApplication: sourceApplication)
-//        return true
-//    }
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        VK.process(url: url, sourceApplication: sourceApplication)
+        return true
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
